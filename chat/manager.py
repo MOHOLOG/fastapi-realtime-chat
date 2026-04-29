115ab25b9ac441b7d3bb4e99125b79fa96d0ebfa
MOHOLOG <opexxx210@gmail.com>
2026-04-29 19:01:34 +0300

    feat: implement sliding window rate limiter for websockets

--- chat/manager.py
+++ chat/manager.py
@@ -1,47 +1,25 @@
 from fastapi import WebSocket
+from user.models import User
+from message.models import Message
 from message.schemas import MessageResponse
-from collections import deque
-import time
+from user.schemas import UserResponse
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: dict[str, dict] = dict()
+        self.active_connections = dict()
     
     async def connect(self, 
                       username: str,
                       websocket: WebSocket):
         await websocket.accept()
-        self.active_connections[username] ={
-            "websocket": websocket,
-            "timestamps": deque()
-        }
+        self.active_connections[username] = websocket
     
     def disconnect(self, username: str):
         del self.active_connections[username]
-
-
-    async def is_rate_limited(self, username: str) -> bool:
-        user_data = self.active_connections.get(username)
-        if not user_data:
-            return False
-
-        timestamps = user_data["timestamps"]
-        now = time.time()
-        window_size = 10 
-        max_messages = 5 
-
-        while timestamps and timestamps[0] < now - window_size:
-            timestamps.popleft()
-
-        if len(timestamps) >= max_messages:
-            return True
-
-        timestamps.append(now)
-        return False
     
     async def broadcast(self, 
                         username: str,
                         message: MessageResponse):
         for user, connection in self.active_connections.items():
             if user != username:
-                await connection["websocket"].send_json(message.model_dump(mode="json"))
+                await connection.send_json(message.model_dump(mode="json"))
